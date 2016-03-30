ab78e0d11f19a7484a406f86fe507eb879533fa7
zenper <zenperton@gmail.com>
2016-03-30 10:11:46 +0700

    Added files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// JavaScript Document
+document.getElementById("foot001").innerHTML ="<p> &copy"  + new Date().getFullYear() + " Zenper Ton. All right reserver. </p> " ; 
+
+
+
+document.getElementById("nav01").innerHTML =
+"<ul id='menu'>" +
+"<li><a href='index.html'> Home </a></li>" +
+"<li><a href='customer.html'> Data </a></li>" +
+" <li><a href='about.html'> About</a></li>" + 
+"</ul>";
